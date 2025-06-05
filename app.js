d6858161336f536a4e33bf7819374ab6de9d2b8e
Kecolve <keclora@gmail.com>
2025-06-04 23:37:06 -0500

    Commit 3: Comentario y llamada a saludar()

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ console.log("Cambio 1: Hola desde el primer commit de prueba.");
 function saludar() {
     console.log("Cambio 2: Esta es una función de saludo.");
 }
+
+// Cambio 3: Llamamos a la función saludar
+saludar();
